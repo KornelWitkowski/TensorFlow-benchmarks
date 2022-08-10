fe5fa3f9486120d47cdeec9a9204676bfd8ec784
KornelWitkowski <80117782+KornelWitkowski@users.noreply.github.com>
2022-08-10 23:13:21 +0200

    MonitorTraining added

--- utils.py
+++ utils.py
@@ -2,6 +2,53 @@ import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+from IPython.display import clear_output
+from tensorflow.keras.callbacks import Callback
+
+
+class MonitorTraining(Callback):
+
+    def on_train_begin(self, _):
+        self.loss = []
+        self.accuracy = []
+        self.val_loss = []
+        self.val_accuracy = []
+        plt.style.use("ggplot")
+
+    def on_epoch_end(self, epoch, logs):
+        self.loss.append(logs['loss'])
+        self.accuracy.append(logs['accuracy'])
+        self.val_loss.append(logs['val_loss'])
+        self.val_accuracy.append(logs['val_accuracy'])
+
+        if len(self.loss) > 1:
+            clear_output(wait=True)
+            self.figure, self.axes = plt.subplots(1, 2, figsize=(16, 9))
+            current_epoch = len(self.loss)
+
+            N = np.arange(1, current_epoch + 1)
+
+            self.figure.suptitle(f"Epoch: {current_epoch}", fontsize=16)
+
+            self.axes[0].plot(N, self.loss, color="red")
+            self.axes[0].scatter(N, self.loss, color="red", label="Train")
+            self.axes[0].plot(N, self.val_loss, color="blue")
+            self.axes[0].scatter(N, self.val_loss, color="blue", label="Test")
+            self.axes[0].set_title("Loss", fontsize=14)
+            self.axes[0].set_xlabel("Epoch")
+            self.axes[0].set_ylabel("Loss")
+            self.axes[0].legend(fontsize=14)
+
+            self.axes[1].plot(N, self.accuracy, color="red")
+            self.axes[1].scatter(N, self.accuracy, color="red", label="Train")
+            self.axes[1].plot(N, self.val_accuracy, color="blue")
+            self.axes[1].scatter(N, self.val_accuracy, color="blue", label="Test")
+            self.axes[1].set_title("Accuracy", fontsize=14)
+            self.axes[1].set_xlabel("Epoch")
+            self.axes[1].set_ylabel("Accuracy")
+            self.axes[1].legend(fontsize=14)
+
+            plt.show()
 
 
 def create_model_checkpoint(model_name, save_path="models"):
@@ -41,4 +88,3 @@ def get_images_and_labels_from_dataset(ds):
     images = np.asarray([image for image, label in ds])
     labels = np.asarray([label for image, label in ds])
     return images, labels
-
